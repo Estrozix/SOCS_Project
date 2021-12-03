83da4f90b8e1a5650214bfeb7dff54fae4bb3e10
Darakhsh1999 <darakhsh@student.chalmers.se>
2021-12-03 12:38:57 +0100

    -added a main (wrapper) - added vaccination status and another column in population matrix that stores vaccination time - added vaccinaion(t) vector in output

---
+++ Main.m
@@ -0,0 +1,28 @@
+% Main 
+
+clear,clc,clf
+
+% TODO:
+
+% vaccinated status
+% move probability depends on illness
+% alpha increases with time 
+% Hospitalized status
+% dynamic/dependent parameters (mu,gamma) etc
+% add Exposed status
+% vaccine dosage
+% add age property
+% vacination rate, vaccination program, vaccination positivity 
+% vaccine decay  rate
+% vaccination percentage variable
+
+time = 3000;
+[S, I, R, D, V] = simulateSIR(0.8,0.01,0.7,0.005,0,0.001,time);
+
+plot(1:time,S)
+hold on
+plot(1:time,I)
+plot(1:time,R)
+plot(1:time,D)
+plot(1:time,V)
+legend("Susceptible","Infected","Recovered","Dead","Vaccinated")
--- simulateSIR.m
+++ simulateSIR.m
@@ -1,5 +1,7 @@
 % runs at most end_time iterations. if end_time=0, only stop when disease is dead.
-function [S, I, R, D] = simulateSIR(beta,gamma,d,mu,alpha,end_time)
+function [S, I, R, D, V] = simulateSIR(beta,gamma,d,mu,alpha,sigma,end_time)
+
+% Simulation Parameters
 latticeN = 100;
 individuals = 1000;
 initial_infected_no = round(0.01*individuals);
@@ -8,26 +10,35 @@ recovery_rate = gamma;
 infect_rate = beta;
 mortality_rate = mu;
 deimmunization_rate = alpha;
-population = zeros(individuals,4); % store (status,pos_x,pos_y,linear_index)
-% 0 = empty
+vaccination_rate = sigma;
+
+
+% Initialize population
+% store (status,pos_x,pos_y,linear_index,vaccination time)
+population = zeros(individuals,5); 
 % 1 = susceptible
 % 2 = infected
 % 3 = recovered
 % 4 = dead
+% 5 = vaccinated (immune)
 population(:,2:3) = randi([0,latticeN],individuals,2);
 population(:,1) = 1;
 population(1:initial_infected_no,1) = 2;
 
+
+% Initialize data
 S = zeros(1,end_time);
 I = zeros(1,end_time);
 R = zeros(1,end_time);
 D = zeros(1,end_time);
+V = zeros(1,end_time);
 I(1) = initial_infected_no;
 S(1) = individuals-initial_infected_no;
 
-% don't stop if end_time == 0
+
+% Main simulation
 t = 1;
-while t ~= end_time
+while t ~= end_time % don't stop if end_time == 0
     % move step
     will_move = rand(individuals,1) < move_probability;
     directions = [+1,0; -1,0; 0,+1; 0,-1];
@@ -40,11 +51,17 @@ while t ~= end_time
     % infection step, almost certainly most of the computation time
     infected = find(population(:,1) == 2);
     for i = 1:length(infected)
-        if rand() < infect_rate
+        if rand < infect_rate
             local_sus = population(:,1) == 1 & population(:,4) == population(infected(i),4);
             population(local_sus,1) = 2;
         end
     end
+
+    % vaccinate step
+    vaccination_condition =  (rand(individuals,1) < sigma & population(:,1) == 1);
+    population(vaccination_condition,1) = 5;
+    population(vaccination_condition,5) = t;
+
 
     % recovery step
     recover_condition = (rand(individuals,1) < recovery_rate & population(:,1) == 2);
@@ -58,11 +75,13 @@ while t ~= end_time
     deimmun_condition = (rand(individuals,1) < deimmunization_rate & population(:,1) == 3);
     population(deimmun_condition,1) = 1;
     
+    % Update data
     t = t + 1;
     S(t) = sum(population(:,1) == 1);
     I(t) = sum(population(:,1) == 2);
     R(t) = sum(population(:,1) == 3);
     D(t) = sum(population(:,1) == 4);
+    V(t) = sum(population(:,1) == 5);
     % check for disease extinction
     if I(t) == 0
         if end_time > 0
@@ -70,9 +89,10 @@ while t ~= end_time
             I((t+1):end) = I(t);
             R((t+1):end) = R(t);
             D((t+1):end) = D(t);
+            V((t+1):end) = V(t);
         end
         break;
     end
-end
+end % end while
 
-end
+end % end function

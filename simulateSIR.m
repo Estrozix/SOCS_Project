8b8dc690092222eaf6ce3558a58c8d8e70ad54a9
CookieShade <CookieShade@users.noreply.github.com>
2021-12-04 14:55:34 +0100

    fix parens in vaccination step

--- simulateSIR.m
+++ simulateSIR.m
@@ -74,7 +74,7 @@ while t ~= end_time % don't stop if end_time == 0
     end
 
     % vaccinate step
-    vaccination_condition =  (rand(individuals,1) < vaccination_rate & population(:,1) == Status.S & ((t - population(:, 5)) > vacc_interval) | population(:, 5) == 0);
+    vaccination_condition =  (rand(individuals,1) < vaccination_rate & population(:,1) == Status.S & (((t - population(:, 5)) > vacc_interval) | population(:, 5) == 0));
     population(vaccination_condition,1) = Status.V;
     population(vaccination_condition,5) = t;
 

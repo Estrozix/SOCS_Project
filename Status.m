f8d09c614e3876b111755d7cff2d4d60f837113b
skrompf <erikbz@outlook.com>
2021-12-08 13:49:04 +0100

    comments

--- Status.m
+++ Status.m
@@ -1,10 +1,10 @@
 classdef Status < double
     enumeration
-        S (0) 
-        E (1)
-        I (2)
-        R (3)
-        D (4)
-        V (5)
+        S (0) %suceptible
+        E (1) %exposed
+        I (2) %infected
+        R (3) %recovered
+        D (4) %dead
+        V (5) %vaccinated
     end
 end

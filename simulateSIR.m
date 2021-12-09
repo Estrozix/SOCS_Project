6f583a37fe01d6991f8e66a20a6c6a852643f914
Darakhsh1999 <darakhsh@student.chalmers.se>
2021-12-09 18:26:45 +0100

    Made 3 seperate functions for moving population, updating latticeMatrix and propagating infection

--- simulateSIR.m
+++ simulateSIR.m
@@ -19,8 +19,8 @@ arguments
     options.rho_a (1, 1) double
     options.time_delay
 end
-%beta,gamma,d,mu,alpha,sigma,end_time
-% Simulation Parameters
+
+% Simulation parameters
 latticeN = options.latticeN;
 individuals = options.individuals;
 initial_infected_no = round(0.01*individuals);
@@ -39,14 +39,7 @@ rho_a = options.rho_a;
 time_delay = options.time_delay;
 
 % Initialize population
-% store (status,pos_x,pos_y,linear_index,vaccination time)
-% S (0) %suceptible
-% E (1) %exposed
-% I (2) %infected (symptomatic)
-% A (3) %infected (asymptomatic)
-% R (4) %recovered
-% D (5) %dead
-% V (6) %vaccinated
+% population: (status,pos_x,pos_y,linear_index,vaccination time)
 population = zeros(individuals,5);
 population(:,1) = Status.S;
 population(1:initial_infected_no,1) = Status.I;
@@ -55,7 +48,7 @@ population(:,4) = population(:,2) + (population(:,3)-1)*latticeN;
 population(:, 5) = 0;
 
 
-% initialize lattice tensor
+% Initialize lattice tensor
 latticeMatrix = zeros(latticeN^2,10);
 for i = 1:individuals
     lindex = population(i,4);
@@ -81,98 +74,21 @@ t = 1;
 infection_time = 0;
 while t ~= end_time % don't stop if end_time == 0
 
-    % move step
-    will_move = rand(individuals,1) < move_probability & population(:,1) ~= Status.D;
-    directions = [+1,0; -1,0; 0,+1; 0,-1];
-    chosen_directions = directions(randi(4,individuals,1),:);
-    temporaryLindex = population(:,4);
-    population(:,2) = population(:,2) + will_move.*chosen_directions(:,1);
-    population(:,3) = population(:,3) + will_move.*chosen_directions(:,2);
-    population(:,2:3) = mod(population(:,2:3)-1, latticeN)+1;
-    population(:,4) = population(:,2) + (population(:,3)-1)*latticeN;
 
-    for i = 1:individuals  % update latticeMatrix
-        if will_move(i)
-
-            % remove their previous step
-            tempLindex = temporaryLindex(i);
-            removeIndex = find(latticeMatrix(tempLindex,:) == i,1);
-            latticeMatrix(tempLindex,removeIndex) = 0;
-
-            % add their new position
-            lindex = population(i,4);
-            freeSlot = find(latticeMatrix(lindex,:) == 0,1);
-            latticeMatrix(lindex,freeSlot) = i;
-        end
-    end
+    % Move population & update latticeMatrix
+    [population,latticeMatrix] = MovePopulation(population,...
+        latticeMatrix, individuals, latticeN, move_probability);
 
 
-
-%     % infection step, almost certainly most of the computation time
-%             starttime = tic;
-%             infected = find(population(:,1) == Status.I | population(:,1) == Status.A);
-%     
-%             for i = 1:length(infected)
-%                 if rand < infect_rate
-%                     local_sus = population(:,1) == Status.S & population(:,4) == population(infected(i),4);
-%                     population(local_sus,1) = Status.E;
-%                 end
-%             end
-%             infection_time = infection_time + toc(starttime);
-
-
-
-
-%     improved infection step
+    % Improved infection step
     starttime = tic;
-    infected = find(population(:,1) == Status.I | population(:, 1) == Status.A);
-    for i = 1:length(infected)
-        if rand < infect_rate
-            q = infected(i);
-            lindexInfected = population(q,4);
-            local_indices = nonzeros(latticeMatrix(lindexInfected,:));
-
-
-            for j = 1:length(local_indices)
-                if population(local_indices(j),1) == Status.S
-                    population(local_indices(j),1) = Status.E;
-                end
-            end
-
-        end
-    end
+    population = PropagateInfection(population, latticeMatrix, infect_rate);
     infection_time = infection_time + toc(starttime);
 
 
+    % Update status
+    population = StatusUpdate(population,options,t);
 
-    % exposed step
-    exposed_condition = (rand(individuals, 1) < inc_factor & population(:, 1) == Status.E);
-    asymptomatic_condition = exposed_condition & (rand(individuals, 1) < rho_a);
-
-    % Temporarily set all symptomatic
-    population(exposed_condition, 1) = Status.I;
-
-    % Set some of the symptomatic to asymptomatic
-    population(asymptomatic_condition, 1) = Status.A;
-
-    % vaccinate step
-    vaccination_condition =  (rand(individuals,1) < vaccination_rate & population(:,1) == Status.S & (((t - population(:, 5)) > vacc_interval) | population(:, 5) == 0));
-    population(vaccination_condition,1) = Status.V;
-    population(vaccination_condition,5) = t;
-
-    
-    % recovery step (both symptomatic and asymptomatic)
-    recover_condition = (rand(individuals,1) < recovery_rate & (population(:,1) == Status.I | population(:, 1) == Status.A));
-    population(recover_condition,1) = Status.R;
-
-    % death step (only sympomatic die)
-    death_condition = (rand(individuals,1) < mortality_rate & population(:,1) == Status.I);
-    population(death_condition,1) = Status.D;
-
-    % deimmunization
-    deimmun_condition = (rand(individuals,1) < deimmunization_rate & population(:,1) == Status.R);
-    vacc_deimmun_condition = (rand(individuals,1) < vacc_deimmun_rate & population(:,1) == Status.V);
-    population(deimmun_condition | vacc_deimmun_condition,1) = Status.S;
 
     % Update data
     t = t + 1;
@@ -184,7 +100,7 @@ while t ~= end_time % don't stop if end_time == 0
     V(t) = sum(population(:,1) == Status.V);
     E(t) = sum(population(:,1) == Status.E);
 
-    % check for disease extinction
+    % Check for disease extinction
     if I(t) == 0 && A(t) == 0
         if end_time > 0
             S((t+1):end) = S(t);
@@ -197,7 +113,8 @@ while t ~= end_time % don't stop if end_time == 0
         end
         break;
     end
-    
+
+    % Animation plotting
     if show_scatter
         suceptible_index = find(population(:,1) == 0);
         exposed_index = find(population(:,1) == 1);
@@ -205,7 +122,7 @@ while t ~= end_time % don't stop if end_time == 0
         recovered_index = find(population(:,1) == 3);
         dead_index = find(population(:,1) == 4);
         vaccinated_index = find(population(:,1) == 5);
-        
+
         scatter(population(dead_index, 2), population(dead_index, 3), 15, "black", "filled");
         hold on
         scatter(population(suceptible_index, 2), population(suceptible_index, 3), 15, [0.3010 0.7450 0.9330], "filled");
@@ -217,7 +134,10 @@ while t ~= end_time % don't stop if end_time == 0
         legend("suceptible","exposed","infected","recovered","dead","vaccinated");
         pause(time_delay);
     end
-    
+
 end % end while
+
 fprintf('Infection runtime: %.3f seconds\n', infection_time);
 end % end function
+
+

50242cb728af5317dd064e453b271c688f5d8996
CookieShade <CookieShade@users.noreply.github.com>
2021-12-11 17:47:53 +0100

    actually real alphas

--- firstAnalysis.m
+++ firstAnalysis.m
@@ -3,7 +3,6 @@
 clear, clc, clf
 
 % Set parameters, vaccine interval every 5 months
-% (1-alpha)^140 = 0.7 => alpha typ 0.0025
 time = 365 * 24 * 5;
 starttime = tic;
 [S, I, A, R, D, V, E, C, vacced, doses] = simulateSIR(...
@@ -11,8 +10,8 @@ starttime = tic;
     gamma = 0.006,... % set
     d = 0.8, ...
     mu = 0.00006, ... % set
-    alpha_nat = 0.0025, ... % set to 30 % loss after 20 weeks
-    alpha_vacc = 0.0025, ... % set to 30 % loss after 20 weeks
+    alpha_nat = -expm1(log(0.7)/(140*24)), ... % set to 30 % loss after 20 weeks
+    alpha_vacc = -expm1(log(0.7)/(140*24)), ... % set to 30 % loss after 20 weeks
     sigma = 0.0001, ... % set
     rho_a = 0.25,... % set
     end_time = time, ...

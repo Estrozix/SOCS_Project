d8c3a98d3fb5605bb96c3ad73abcaa9e2dde765f
Estrozix <estrozix@gmail.com>
2021-12-13 18:40:56 +0100

    random updates

--- ParameterSpaceSimulation.m
+++ ParameterSpaceSimulation.m
@@ -2,37 +2,41 @@
 clear,clc
 
 averages = 10;
-time = 3000;
+time = 365 * 24 * 5;
 
 % parameters to vary
-vaccineIntervals = [100:50:400];
+vaccineIntervals = (24*30) .* [1:24];
 
 tic
 D_loop = zeros(averages,length(vaccineIntervals));
+Cm_loop = zeros(averages,length(vaccineIntervals));
 parfor i = 1:length(vaccineIntervals)
     for run = 1:averages
-        [S, I, A, R, D, V, E, C, vacced, doses] = simulateSIR(...
+        [S, I, A, R, D, V, E, C, Cm, vacced, doses, true_end] = simulateSIR(...
             show_scatter = false, ...
             time_delay = 0.1, ...
             end_time = time, ...
             gamma = 0.006,... % set
             rho_a = 0.25,... % set
             mu = 0.00006, ... % set
-            inc_factor = 0.08, ... % set
+            inc_factor = 0.008, ... % set
             d = 0.8, ... % set
-            beta = 0.4,...
-            alpha_nat = 0.01, ...
-            alpha_vacc = 0.01, ...
-            sigma = 0.0001, ...
+            beta = 0.03,...
+            alpha_nat = 1/(24*30*5), ...
+            alpha_vacc = 1/(24*30*5), ...
+            sigma = 1/(24*30*6), ...
             vacc_interval = vaccineIntervals(i) ...
             );
         D_loop(run,i) = D(end);
+        Cm_loop(run,i) = Cm(end);
     end
 end
 toc
 
-
+Cm_mean = mean(Cm_loop);
 D_mean = mean(D_loop);
 
 plot(vaccineIntervals,D_mean)
+hold on
+plot(vaccineIntervals,Cm_mean)
 
--- plotData.m
+++ plotData.m
@@ -31,3 +31,34 @@ width=1000;
 height=500;
 set(gcf,'position',[x0,y0,width,height])
 
+%%
+vaccineMonths = vaccineIntervals / (24*30);
+plot(vaccineMonths,Cm_mean, 'LineWidth', 2)
+legend("Cases")
+ylabel("N")
+xlabel("Vaccine Interval (Months)")
+title("Five year simulation with sigma = 1/(24*30*6)")
+
+grid on
+
+x0=10;
+y0=10;
+width=1000;
+height=500;
+set(gcf,'position',[x0,y0,width,height])
+
+%%
+vaccineMonths = vaccineIntervals / (24*30);
+plot(vaccineMonths,D_mean, 'LineWidth', 2)
+legend("Deaths")
+ylabel("N")
+xlabel("Vaccine Interval (Months)")
+title("Five year simulation with sigma = 1/(24*30*6)")
+
+grid on
+
+x0=10;
+y0=10;
+width=1000;
+height=500;
+set(gcf,'position',[x0,y0,width,height])

6c10a4a83b0bacafac0161cbfb7430cfac4e48bf
Darakhsh1999 <darakhsh@student.chalmers.se>
2021-12-13 18:19:43 +0100

    output variables are truncaded if disease dies out

--- simulateSIR.m
+++ simulateSIR.m
@@ -1,7 +1,6 @@
 % runs at most end_time iterations. if end_time=0, only stop when disease is dead.
 
-function [S, I, A, R, D, V, E, C, Cm, no_vacced_once, total_no_of_doses] = simulateSIR(options)
-
+function [S, I, A, R, D, V, E, C, Cm, nV, nVD] = simulateSIR(options)
 
 arguments
     options.latticeN double = 100
@@ -21,8 +20,6 @@ arguments
     options.time_delay
 end
 
-totalstarttime = tic;
-
 % Simulation parameters
 latticeN = options.latticeN;
 individuals = options.individuals;
@@ -41,18 +38,16 @@ show_scatter = options.show_scatter;
 rho_a = options.rho_a;
 time_delay = options.time_delay;
 
+
 % Initialize population
-% population: (status,pos_x,pos_y,linear_index,vaccination time, # doses taken, times infected)
+% population: status(1),pos_x(2),pos_y(3),linear_index(4),vaccination time(5), # doses taken(6), times infected(7)
 population = zeros(individuals,7);
 population(:,1) = Status.S;
 population(1:initial_infected_no,1) = Status.I;
-
-% Update infection times
-population(1:initial_infected_no,7) = 1;
-
 population(:,2:3) = randi([1,latticeN],individuals,2);
 population(:,4) = population(:,2) + (population(:,3)-1)*latticeN;
-population(:, 5) = 0;
+population(:,5) = 0;
+population(1:initial_infected_no,7) = 1;
 
 
 % Initialize lattice tensor
@@ -74,11 +69,10 @@ V = zeros(1,end_time);
 E = zeros(1,end_time);
 C = zeros(1,end_time);
 Cm = zeros(1,end_time);
+nV = zeros(1,end_time); % # vaccinated
+nVD = zeros(1,end_time); % # vaccine doses
 I(1) = initial_infected_no;
 S(1) = individuals-initial_infected_no;
-
-no_vacced_once = zeros(1,end_time);
-total_no_of_doses = zeros(1,end_time);
 
 
 % Main simulation
@@ -109,24 +103,24 @@ while t ~= end_time % don't stop if end_time == 0
     V(t) = sum(population(:,1) == Status.V);
     E(t) = sum(population(:,1) == Status.E);
     C(t) = sum(population(:,7) > 0);
-    Cm(t) = sum(population(:, 7));
-    no_vacced_once(t) = sum(population(:,6) > 0);
-    total_no_of_doses(t) = sum(population(:,6));
+    Cm(t) = sum(population(:,7));
+    nV(t) = sum(population(:,6) > 0);
+    nVD(t) = sum(population(:,6));
 
     % Check for disease extinction
     if I(t) == 0 && A(t) == 0 && E(t) == 0
         if end_time > 0
-            S((t+1):end) = S(t);
-            I((t+1):end) = I(t);
-            A((t+1):end) = A(t);
-            R((t+1):end) = R(t);
-            D((t+1):end) = D(t);
-            V((t+1):end) = V(t);
-            E((t+1):end) = E(t);
-            C((t+1):end) = C(t);
-            Cm((t+1):end) = Cm(t);
-            no_vacced_once((t+1):end) = no_vacced_once(t);
-            total_no_of_doses((t+1):end) = total_no_of_doses(t);
+            S = S(1:t);
+            I = I(1:t);
+            A = A(1:t);
+            R = R(1:t);
+            D = D(1:t);
+            V = V(1:t);
+            E = E(1:t);
+            C = C(1:t);
+            Cm = Cm(1:t);
+            nV = nV(1:t);
+            nVD = nVD(1:t);
         end
         break;
     end
